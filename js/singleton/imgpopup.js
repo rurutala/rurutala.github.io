cf33a1625ca2decccf7a3235ac630b8ee90d761d
rurutala <rurutala.ch@gmail.com>
2023-12-15 16:03:59 +0900

    works_commit

---
+++ js/singleton/imgpopup.js
@@ -0,0 +1,29 @@
+var works_number;
+$(function(){
+	// 変数に要素を入れる
+	var open = $('.modal-open'),
+		close = $('.modal-close'),
+		container = $('.modal-container');
+
+	//開くボタンをクリックしたらモーダルを表示する
+	open.on('click',function(){	
+		container.eq(works_number).addClass('active');
+		return false;
+	});
+
+	//閉じるボタンをクリックしたらモーダルを閉じる
+	close.on('click',function(){	
+		container.eq(works_number).removeClass('active');
+	});
+
+	//モーダルの外側をクリックしたらモーダルを閉じる
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.modal-body').length) {
+			container.eq(works_number).removeClass('active');
+		}
+	});
+});
+
+function clickFunction(value){
+    works_number = value- 1;
+}
